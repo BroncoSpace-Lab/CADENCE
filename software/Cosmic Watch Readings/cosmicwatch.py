f5993d3bfe0d54f40d7b56caeb9a768293d13043
PragunB <124644904+PragunB@users.noreply.github.com>
2025-07-22 11:36:45 -0700

    added calibration variable
    will use this in future to make a raw sipm voltage function

--- software/Cosmic Watch Readings/cosmicwatch.py
+++ software/Cosmic Watch Readings/cosmicwatch.py
@@ -5,10 +5,23 @@ import analogio
 
 cosmic_watch_output = analogio.AnalogIn(board.COSMIC_ANALOG)
 counter = 0
-SIGNAL_THRESHOLD = 1000
+SIGNAL_THRESHOLD = 1500
+
+cal = [-9.085681659276021e-27, 4.6790804314609205e-23, -1.0317125207013292e-19,
+        1.2741066484319192e-16, -9.684460759517656e-14, 4.6937937442284284e-11, 
+        -1.4553498837275352e-08, 2.8216624998078298e-06, -0.000323032620672037, 
+        0.019538631135788468, -0.3774384056850066, 12.324891083404246]
+
+cal_max = 1023
 
 def get_voltage(raw):
     return (raw * 3.3) / 65536
+
+def get_sipm_voltage(raw):
+    voltage = 0
+    for i in range(cal)/range(float):
+
+
 
 try:
     while True:
@@ -18,7 +31,7 @@ try:
         if cosmic_watch_output.value > SIGNAL_THRESHOLD:
             counter += 1
             print("Particle has been detected!")
-            
+
 except KeyboardInterrupt:
     print("Program has been interrupted")
     print("============================")
